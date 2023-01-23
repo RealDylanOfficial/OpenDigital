0d4c297f8e2bb6fa4d38eb05081ca530f90b0187
h57819dw <dylan.williamson@student.manchester.ac.uk>
2023-01-23 01:36:34 +0000

    created users and posts migrations

---
+++ database/migrations/2023_01_13_033510_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("username", 128)->unique();
+            $table->string("password_hash", 24);     //Salt is store in password hash using Bcrypt
+            $table->string("profile_picture");       //relative filepath to picture
+            $table->string("email")->unique();
+            $table->timestamps();
+            //Also note that when running migrations, a personal access token table is created as well. This is part of laravel Sanctum and I can disable it but we may wish to use it in the future
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
