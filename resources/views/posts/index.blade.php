5319fe339c89f639596b5229b076705475520fb9
h57819dw <dylan.williamson@student.manchester.ac.uk>
2023-03-22 13:23:08 +0000

    Revert "added like button styling on posts" I tried to keep important files but I don't know This reverts commit ad041befa3e3303813979b341810b2d9cc1d03bd.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,6 @@
     <meta name="_token" content="{{ csrf_token() }}">
 
     <link rel="icon" href="{{asset("images/logo.png")}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>OpenDigital - Posts</title>
 
     @viteReactRefresh
